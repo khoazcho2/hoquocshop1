58509443ea2e77ff578a41fd42320bcaabfefb08
khoazcho2 <vot835848@gmail.com>
2026-02-25 11:52:50 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// Code chỉ sử dụng nạp thẻ được tại khothegame.com thôi nhé 
+// Kết nối API => Đổi thẻ cào => Bỏ trống Đường dẫn nhận dữ liệu (Callback Url)
+date_default_timezone_set('Asia/Ho_Chi_Minh');
+$partner_id = "4468512341";  // kết nối api tại khothegame.com
+$partner_key = "a6242c6bc57135073de71f946cfef66e";  // kết nối api tại khothegame.com
+$botToken = "8480479854:AAGjAuqNhJnrt64yLRl-wid5kP6ycFGIBQ0";//thay bằng token bot @BotFather
+$chatId = "8480479854";// thay bằng chatid của nick @Accclone5k_bot
+?>
